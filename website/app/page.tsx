1e7db18006381cd2d736e65ae2f114e64b823572
Anasseurukoo <141363131+Anasseurukoo@users.noreply.github.com>
2026-07-19 08:50:37 +0100

    feat(website): add services AR about contact and mobile actions

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,16 +1,31 @@
 import Collections from "../components/Collections";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import {
+  AboutSection,
+  AppExperienceSection,
+  ContactSection,
+  HomeFooter,
+  MobileStickyBar,
+  ServicesSection,
+} from "../components/PremiumSections";
 
 export default function Home() {
   return (
     <>
       <Navbar />
 
-      <main>
+      <main id="top" className="pb-20 md:pb-0">
         <Hero />
         <Collections />
+        <ServicesSection />
+        <AppExperienceSection />
+        <AboutSection />
+        <ContactSection />
       </main>
+
+      <HomeFooter />
+      <MobileStickyBar />
     </>
   );
 }
